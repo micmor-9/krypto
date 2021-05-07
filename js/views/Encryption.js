2dfaa7df4eaa8f06f2cc93478aceda5cbc3e2f0b
micmor-9 <michelemorgigno@gmail.com>
2021-05-07 18:34:49 +0200

    Added File upload in Encryption

--- js/views/Encryption.js
+++ js/views/Encryption.js
@@ -5,6 +5,7 @@
 import AbstractView from "./AbstractView.js";
 
 const maxMessageLength = 200;
+const keyLengths = [128,192,256];
 
 export default class Encryption extends AbstractView {
   constructor(params) {
@@ -14,6 +15,8 @@ export default class Encryption extends AbstractView {
 
   #encryptionHandler() {
     $("#encryptionForm").submit(function (event) {
+      //TODO form validation
+      
       event.preventDefault();
       var values = {};
       $.each($(this).serializeArray(), function (i, field) {
@@ -28,26 +31,43 @@ export default class Encryption extends AbstractView {
     return (
       `<div class="container">
     <h1>Encryption</h1>
-    <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt facere dolorem quaerat vitae, iure eum voluptatum libero.</p>
+    <p class="lead">Advanced Encryption Standard(AES) is a symmetric encryption algorithm. AES is the industry standard as of now as it allows 128 bit, 192 bit and 256 bit encryption. Symmetric encryption is very fast as compared to asymmetric encryption and are used in systems such as database system.</p>
     <form name="encryptionForm" id="encryptionForm" method="post" class="col-12 col-lg-8 col-xl-5 mr-auto my-3">
     <div class="row my-1">
+      <label for="encryptionObjectType" class="col-sm-4 col-form-label my-2">Choose </label>
+      <div class="col my-2">
+        <div class="btn-group" role="group" aria-label="Encryption Object Type">
+          <input type="radio" class="btn-check" name="encryptionObjectType" id="encObjMessage" value="msg" autocomplete="off" checked>
+          <label class="btn btn-outline-primary" for="encObjMessage">Message</label>    
+          <input type="radio" class="btn-check" name="encryptionObjectType" id="encObjFile" value="file" autocomplete="off">
+          <label class="btn btn-outline-primary" for="encObjFile">File</label>
+        </div>
+      </div>
+    </div>
+    <div class="row my-1 encryption-message">
       <div class="col my-2">
         <label for="encryptionMessage" class="form-label">Your message</label>
         <textarea class="form-control" id="encryptionMessage" name="encryptionMessage" rows="10"></textarea>
-        <span class="character-counter">0/` +
+        <span class="character-counter" id="messageCharacterCounter">0/` +
       maxMessageLength +
       `</span>
+      </div>
+    </div>
+    <div class="row my-1">
+      <label for="encryptionFile" class="encryption-file col-sm-4 col-form-label my-2">Upload your file</label>
+      <div class="col my-2"> 
+        <input type="file" class="encryption-file form-control" id="encryptionFile" name="encryptionFile">
       </div>
     </div>
     <div class="row my-1">
       <label for="encryptionKeyLength" class="col-sm-4 col-form-label my-2">Key Length</label>
       <div class="col my-2">
         <div class="btn-group" role="group" aria-label="Key Length input group">
-          <input type="radio" class="btn-check" name="encryptionKeyLength" id="keyLength128" autocomplete="off" checked>
+          <input type="radio" class="btn-check" name="encryptionKeyLength" id="keyLength128" value="128" autocomplete="off" checked>
           <label class="btn btn-outline-primary" for="keyLength128">128 bit</label>    
-          <input type="radio" class="btn-check" name="encryptionKeyLength" id="keyLength192" autocomplete="off">
+          <input type="radio" class="btn-check" name="encryptionKeyLength" id="keyLength192" value="192" autocomplete="off">
           <label class="btn btn-outline-primary" for="keyLength192">192 bit</label>    
-          <input type="radio" class="btn-check" name="encryptionKeyLength" id="keyLength256" autocomplete="off">
+          <input type="radio" class="btn-check" name="encryptionKeyLength" id="keyLength256" value="256" autocomplete="off">
           <label class="btn btn-outline-primary" for="keyLength256">256 bit</label>
         </div>
       </div>
@@ -59,9 +79,10 @@ export default class Encryption extends AbstractView {
       </div>
     </div>
     <div class="row my-1">
-      <label for="encryptionKey" class="col-sm-4 col-form-label my-2">Your key</label>
+      <label for="encryptionKey" class="col-sm-4 col-form-label">Your key</label>
       <div class="col my-2">
         <input type="text" class="form-control" id="encryptionKey" name="encryptionKey">
+        <span class="character-counter" id="keyCharacterCounter">0/` + keyLengths[0]/8 + `</span>
       </div>
     </div>
     <div class="row my-1">
@@ -76,9 +97,39 @@ export default class Encryption extends AbstractView {
 
   loadScripts() {
     $("#encryptionMessage").on("input", function () {
-      $(".character-counter").html(
+      $("#messageCharacterCounter").html(
         $(this).val().length + "/" + maxMessageLength
       );
+    });
+
+    $("input[name='encryptionKeyLength']").on("change", function () {
+      let keyLength = $(this).val();
+      $('#keyCharacterCounter').html(
+        $("#encryptionKey").val().length + "/" + keyLength/8
+      );
+    });
+
+    $("#encryptionKey").on("input", function () {
+      let keyLength = $("input[name='encryptionKeyLength']").val();
+      $('#keyCharacterCounter').html(
+        $(this).val().length + "/" + keyLength/8
+      );
+    });
+
+    $("input[name='encryptionObjectType']").on("change", function () {
+      let type = $(this).val();
+      switch (type) {
+        case 'msg': 
+          $('.encryption-file').fadeOut();
+          $('.encryption-message').fadeIn();
+          break;
+
+        case 'file':
+          $('.encryption-message').fadeOut();
+          $('.encryption-file').fadeIn();
+          break;
+
+      }
     });
 
     this.#encryptionHandler();
