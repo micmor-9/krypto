637e69996774d41c956e6b75b31eefeca74c11fc
micmor-9 <michelemorgigno@gmail.com>
2021-05-11 17:43:20 +0200

    Added key field valdiation

--- js/views/Encryption.js
+++ js/views/Encryption.js
@@ -4,7 +4,7 @@
 
 import AbstractView from "./AbstractView.js";
 
-const maxMessageLength = 200;
+const maxMessageLength = 400;
 const keyLengths = [128, 192, 256];
 
 export default class Encryption extends AbstractView {
@@ -16,8 +16,16 @@ export default class Encryption extends AbstractView {
   #encryptionHandler() {
     $("#encryptionForm").submit(function (event) {
       //TODO form validation
+      var form = document.getElementById('encryptionForm');
+      
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
 
-      event.preventDefault();
+      form.classList.add("was-validated");
+
+      /* event.preventDefault();
       var values = {};
       $.each($(this).serializeArray(), function (i, field) {
         values[field.name] = field.value;
@@ -28,12 +36,12 @@ export default class Encryption extends AbstractView {
           values["encryptionMessage"],
           values["encryptionKey"]
         )
-      );
+      ); */
     });
   }
 
   generateKey() {
-    let keyLength = ($("input[name='encryptionKeyLength']:checked").val());
+    let keyLength = $("input[name='encryptionKeyLength']:checked").val();
     var salt = CryptoJS.lib.WordArray.random(128 / 8);
     var passphrase = CryptoJS.lib.WordArray.random(16 / 8);
     var generatedKey = CryptoJS.PBKDF2(passphrase, salt, {
@@ -53,12 +61,18 @@ export default class Encryption extends AbstractView {
 
   async getHtml() {
     var date = new Date();
-    var stringDate = date.getFullYear() + '-' + ('0' + (date.getMonth()+1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
+    date.setDate(date.getDate() + 30);
+    var stringDate =
+      date.getFullYear() +
+      "-" +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      "-" +
+      ("0" + date.getDate()).slice(-2);
     return (
       `<div class="container">
     <h1>Encryption</h1>
     <p class="lead">Advanced Encryption Standard(AES) is a symmetric encryption algorithm. AES is the industry standard as of now as it allows 128 bit, 192 bit and 256 bit encryption. Symmetric encryption is very fast as compared to asymmetric encryption and are used in systems such as database system.</p>
-    <form name="encryptionForm" id="encryptionForm" method="post" class="col-12 col-lg-8 col-xl-5 mr-auto my-3">
+    <form name="encryptionForm" id="encryptionForm" method="post" class="col-12 col-lg-8 col-xl-5 mr-auto my-3 needs-validation" novalidate>
     <div class="row my-1">
       <label for="encryptionObjectType" class="col-sm-4 col-form-label my-2">Choose </label>
       <div class="col my-2">
@@ -73,10 +87,11 @@ export default class Encryption extends AbstractView {
     <div class="row my-1 encryption-message">
       <div class="col my-2">
         <label for="encryptionMessage" class="form-label">Your message</label>
-        <textarea class="form-control" id="encryptionMessage" name="encryptionMessage" rows="10"></textarea>
+        <textarea class="form-control" id="encryptionMessage" name="encryptionMessage" rows="10" required></textarea>
         <span class="character-counter" id="messageCharacterCounter">0/` +
       maxMessageLength +
       `</span>
+      <div class="invalid-feedback">The message field cannot be empty.</div>
       </div>
     </div>
     <div class="row my-1">
@@ -101,21 +116,25 @@ export default class Encryption extends AbstractView {
     <div class="row my-1">
       <label for="encryptionTimeout" class="col-sm-4 col-form-label my-2">Timeout</label>
       <div class="col my-2">
-        <input type="date" class="form-control" id="encryptionTimeout" name="encryptionTimeout" value="` + stringDate +`">
+        <input type="date" class="form-control" id="encryptionTimeout" name="encryptionTimeout" value="` +
+      stringDate +
+      `">
       </div>
     </div>
     <div class="row my-1">
       <label for="encryptionKey" class="col-sm-4 col-form-label my-2">Your key</label>
       <div class="col my-2">
         <div class="input-group">
-          <input type="text" class="form-control" id="encryptionKey" name="encryptionKey">
+          <input type="text" class="form-control" id="encryptionKey" name="encryptionKey" maxlength="` + keyLengths[0]/8 + `" required>
           <button class="btn btn-outline-primary" type="button" id="generateKey" data-bs-toggle="tooltip" data-bs-placement="top" title="Generate a key"><i class="bi bi-key-fill"></i></button>
-        </div>
+          <div class="invalid-feedback">The key field cannot be empty.</div>
+          </div>
         <span class="character-counter" id="keyCharacterCounter">0/` +
       keyLengths[0] / 8 +
       `</span>
       </div>
     </div>
+    
     <div class="row my-1">
       <div class="col my-2">
         <button class="btn btn-primary" type="submit">Encrypt</button>
@@ -134,6 +153,7 @@ export default class Encryption extends AbstractView {
     });
 
     $("input[name='encryptionKeyLength']").on("change", () => {
+      $('#encryptionKey').attr('maxlength', $("input[name='encryptionKeyLength']:checked").val()/8);
       this.charCounterUpdate();
     });
 
@@ -146,12 +166,18 @@ export default class Encryption extends AbstractView {
       switch (type) {
         case "msg":
           $(".encryption-file").fadeOut();
+          $('#encryptionFile').attr('required', false);
+
           $(".encryption-message").fadeIn();
+          $('#encryptionMessage').attr('required', true);
           break;
 
         case "file":
           $(".encryption-message").fadeOut();
+          $('#encryptionMessage').attr('required', false);
+
           $(".encryption-file").fadeIn();
+          $('#encryptionFile').attr('required', true);
           break;
       }
     });
