a1c6a929aa818befc825fa9346bc2c685a0ff635
micmor-9 <michelemorgigno@gmail.com>
2021-05-27 12:39:30 +0200

    Database setup and connection

--- components/classes/database.php
+++ components/classes/database.php
@@ -3,27 +3,89 @@
   class Database {
 
     /* CONNECTION PARAMETERS */
-    static $DB_HOST;
-    static $DB_USER;
-    static $DB_PASSWORD;
-    static $DB_NAME;
-    /* -------------------.- */
+    private $DB_HOST;
+    private $DB_USER;
+    private $DB_PASSWORD;
+    private $DB_NAME;
+
+    private $connection;
+    /* --------------------- */
+
+
 
     function __construct() {
       /* Get settings from ini file */
       $path = $_SERVER['DOCUMENT_ROOT'];
       $ini_data = parse_ini_file($path.'/config/db.ini');
-      var_dump($ini_data);
+
+      $this->DB_HOST = $ini_data['DB_HOST'];
+      $this->DB_USER = $ini_data['DB_USER'];
+      $this->DB_PASSWORD = $ini_data['DB_PASSWORD'];
+      $this->DB_NAME = $ini_data['DB_NAME'];
+
+      $this->connection = $this->connect();
+      $sql = 'SHOW TABLES FROM '.$this->DB_NAME;
+      $result = $this->connection->query($sql);
+
+      if($result->num_rows == 0) {
+        if($this->setup()){
+
+        }
+      } else {
+        while ($row = mysqli_fetch_row($result)) {
+          echo "Table: {$row[0]}\n";
+        }
+      }
     }
 
     function setup() {
+      //DB Tables inizialization
+      //Table: user
+      $sql = "CREATE TABLE user ( user_id INT NOT NULL AUTO_INCREMENT , email VARCHAR(30) NOT NULL , first_name VARCHAR(30) NOT NULL , last_name INT(30) NOT NULL , password INT(50) NOT NULL , birthdate DATE NOT NULL , PRIMARY KEY (user_id));";
+      $result = $this->connection->query($sql);
+      
+      if(!$result) {
+        echo 'Error while creating table user. '.$this->connection->error;
+        return false;
+      }
 
+      //Table: key
+      $sql = "CREATE TABLE pass_key ( key_id INT NOT NULL AUTO_INCREMENT , key_value VARCHAR(32) NOT NULL , key_length INT NOT NULL , PRIMARY KEY (key_id));";
+      $result = $this->connection->query($sql);
+      
+      if(!$result) {
+        echo 'Error while creating table key. '.$this->connection->error;
+        return false;
+      }
+
+      //Table: qr_code
+      $sql = "CREATE TABLE qr_code ( qr_id INT NOT NULL AUTO_INCREMENT , reference VARCHAR(100) NOT NULL , key_length INT NOT NULL , PRIMARY KEY (qr_id));";
+      $result = $this->connection->query($sql);
+      
+      if(!$result) {
+        echo 'Error while creating table qr_code. '.$this->connection->error;
+        return false;
+      }
+
+      //Table: ecnrypted_object
+      $sql = "CREATE TABLE encrypted_object ( obj_id INT NOT NULL AUTO_INCREMENT , timeout DATE NOT NULL , content VARCHAR(1000) NULL , download_link VARCHAR(50) NULL , user_id INT NOT NULL, key_id INT NOT NULL, qr_id INT NOT NULL, PRIMARY KEY (obj_id), FOREIGN KEY (user_id) REFERENCES user(user_id), FOREIGN KEY (key_id) REFERENCES pass_key(key_id), FOREIGN KEY (qr_id) REFERENCES qr_code(qr_id))";
+      $result = $this->connection->query($sql);
+      
+      if(!$result) {
+        echo 'Error while creating table encrypted_object. '.$this->connection->error;
+        return false;
+      }
+
+      return true;
     }
 
     function connect() {
-
+      if(!isset($this->DB_HOST) || !isset($this->DB_USER) || !isset($this->DB_PASSWORD) || !isset($this->DB_NAME)) {
+        return false;
+      } else {
+        $mysqli = new mysqli($this->DB_HOST, $this->DB_USER, $this->DB_PASSWORD, $this->DB_NAME);
+        return $mysqli;
+      }
     }
 
   }
-
-?>
